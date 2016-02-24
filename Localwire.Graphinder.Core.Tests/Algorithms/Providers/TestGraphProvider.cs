23ac8b6e4c10a8e8667e98888bb783711dba6cd9
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-02-25 00:07:52 +0100

    Missing graph provider.

---
+++ Localwire.Graphinder.Core.Tests/Algorithms/Providers/TestGraphProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace Localwire.Graphinder.Core.Tests.Algorithms.Providers
+{
+    using System.Collections.Generic;
+    using Graph;
+
+    public class TestGraphProvider : ITestDataProvider<Graph>
+    {
+        public Graph ProvideValid()
+        {
+            return new Graph(new List<string> { "1", "2", "3"});
+        }
+    }
+}
