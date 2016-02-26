c35ff3659c4b03ca83c8f59df8d1714418bdf6cf
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-02-26 23:27:49 +0100

    Initial RouletteStrategy implementation.

--- Localwire.Graphinder.Core/Algorithms/SelectionStrategies/RouletteStrategy.cs
+++ Localwire.Graphinder.Core/Algorithms/SelectionStrategies/RouletteStrategy.cs
@@ -2,23 +2,51 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using GeneticAlgorithm;
 
+    /// <summary>
+    /// Class representing selection strategy based on roulette wheel aka Fitness Proportionate Selection.
+    /// </summary>
+    //TODO: Inject ProblemCriteria?
     public class RouletteStrategy : ISelectionStrategy
     {
+        private readonly Random _random = new Random();
+        private ICollection<Individual> _population;
+        private readonly IDictionary<Individual, double> _individualsWeight = new Dictionary<Individual, double>();
+
+        public void Set(ICollection<Individual> newPopulation)
+        {
+            _population = newPopulation;
+            _population = _population.OrderBy(p => p.SolutionOutcome).ToList();
+            InitializeRoulette();
+        }
+
         public Individual Next()
         {
-            throw new NotImplementedException();
+            if (_population == null || _population.Count == 0)
+                throw new InvalidOperationException("Selection strategy needs to have population set first!");
+            var totalWeightsSkipped = 0.0;
+            foreach (var element in _individualsWeight)
+            {
+                totalWeightsSkipped += element.Value;
+                if (_random.NextDouble() < totalWeightsSkipped)
+                    return element.Key;
+            }
+            return _population.Last();
         }
 
         public Tuple<Individual, Individual> NextCouple()
         {
-            throw new NotImplementedException();
+            return new Tuple<Individual, Individual>(Next(), Next());
         }
 
-        public void Set(ICollection<Individual> newPopulation)
+        private void InitializeRoulette()
         {
-            throw new NotImplementedException();
+            _individualsWeight.Clear();
+            var populationSum = _population.Sum(p => p.SolutionOutcome);
+            foreach(var element in _population)
+                _individualsWeight.Add(element, (double)element.SolutionOutcome/populationSum);
         }
     }
 }
