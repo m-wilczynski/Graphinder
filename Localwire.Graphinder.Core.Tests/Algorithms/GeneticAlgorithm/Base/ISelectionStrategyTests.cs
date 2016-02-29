ec003525aeef4895542f66bb48e10e53f23283de
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-02-29 23:37:14 +0100

    Additional tests for selection strategies.

--- Localwire.Graphinder.Core.Tests/Algorithms/GeneticAlgorithm/Base/ISelectionStrategyTests.cs
+++ Localwire.Graphinder.Core.Tests/Algorithms/GeneticAlgorithm/Base/ISelectionStrategyTests.cs
@@ -4,11 +4,14 @@
     using System.Collections.Generic;
     using Core.Algorithms.GeneticAlgorithm;
     using Core.Algorithms.GeneticAlgorithm.SelectionStrategies;
+    using Providers;
+    using Providers.TestData;
     using Xunit;
 
     public abstract class ISelectionStrategyTests
     {
         protected ISelectionStrategy _strategy;
+        private readonly ITestDataProvider<Individual> _individualProvider = new TestIndividualProvider();
 
         protected ISelectionStrategyTests()
         {
@@ -23,6 +26,23 @@
             });
 
             _strategy.Set(new List<Individual>());
+        }
+
+        [Fact]
+        public void ISelectionStrategy_Next_ThrowsOnEmptyPopulation()
+        {
+            _strategy.Set(new List<Individual>());
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                _strategy.Next();
+            });
+        }
+
+        [Fact]
+        public void ISelectionStrategy_Next_ReturnsProperValueIfOneIndividualPopulation()
+        {
+            _strategy.Set(new List<Individual> { _individualProvider.ProvideValid() });
+            _strategy.Next();
         }
     }
 }
--- Localwire.Graphinder.Core.Tests/Algorithms/GeneticAlgorithm/RouletteStrategyTests.cs
+++ Localwire.Graphinder.Core.Tests/Algorithms/GeneticAlgorithm/RouletteStrategyTests.cs
@@ -1,21 +1,13 @@
 ﻿namespace Localwire.Graphinder.Core.Tests.Algorithms.GeneticAlgorithm
 {
-    using System;
     using Base;
     using Core.Algorithms.GeneticAlgorithm.SelectionStrategies;
-    using Xunit;
 
     public class RouletteStrategyTests : ISelectionStrategyTests
     {
         public RouletteStrategyTests()
         {
             _strategy = new RouletteStrategy();
-        }
-
-        [Fact]
-        public void RouletteStrategy_Next_ReturnsProperValueIfOneIndividualPopulation()
-        {
-            throw new NotImplementedException();
         }
     }
 }
