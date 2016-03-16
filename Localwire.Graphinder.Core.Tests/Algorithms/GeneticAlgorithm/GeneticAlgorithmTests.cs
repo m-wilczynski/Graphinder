9987409edd1b8845096c6b8f07381888c17b56f8
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-03-16 22:13:52 +0100

    GeneticAlgorithm - ctor tests.

--- Localwire.Graphinder.Core.Tests/Algorithms/GeneticAlgorithm/GeneticAlgorithmTests.cs
+++ Localwire.Graphinder.Core.Tests/Algorithms/GeneticAlgorithm/GeneticAlgorithmTests.cs
@@ -1,9 +1,12 @@
 ﻿namespace Localwire.Graphinder.Core.Tests.Algorithms.GeneticAlgorithm
 {
+    using System;
     using Core.Algorithms.GeneticAlgorithm.Setup;
     using Core.Algorithms.GeneticAlgorithm;
+    using Core.Graph;
     using Providers;
     using Providers.SubstituteData;
+    using Xunit;
 
     public class GeneticAlgorithmTests : AlgorithmTests
     {
@@ -12,6 +15,55 @@
         public GeneticAlgorithmTests()
         {
             _algorithm = new GeneticAlgorithm(_graphFactory.ProvideValid(), _problemProvider.ProvideSubstitute(), _operatorsProvider.ProvideSubstitute());
+        }
+
+        [Fact]
+        public void GeneticAlgorithm_ctor_ThrowsOnNullGraph()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new GeneticAlgorithm(null, _problemProvider.ProvideSubstitute(), _operatorsProvider.ProvideSubstitute());
+            });
+        }
+
+        [Fact]
+        public void GeneticAlgorithm_ctor_ThrowsOnInvalidGraph()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new GeneticAlgorithm(new Graph(), _problemProvider.ProvideSubstitute(),
+                    _operatorsProvider.ProvideSubstitute());
+            });
+        }
+
+        [Fact]
+        public void GeneticAlgorithm_ctor_ThrowsOnNullProblem()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new GeneticAlgorithm(_graphFactory.ProvideValid(), null,
+                    _operatorsProvider.ProvideSubstitute());
+            });
+        }
+
+        [Fact]
+        public void GeneticAlgorithm_ctor_ThrowsOnNullGeneticOperators()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new GeneticAlgorithm(_graphFactory.ProvideValid(), _problemProvider.ProvideSubstitute(),
+                    null);
+            });
+        }
+
+        [Fact]
+        public void GeneticAlgorithm_ctor_ThrowsOnInvalidGeneticOperators()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new GeneticAlgorithm(_graphFactory.ProvideValid(), _problemProvider.ProvideSubstitute(),
+                    new GeneticOperators(null, null, null));
+            });
         }
     }
 }
