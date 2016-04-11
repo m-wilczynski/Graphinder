18d0b11dd342fc0cb70239b545293dcf82db21d6
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-04-11 23:01:35 +0200

    Individual and GeneticAlgorithm entities.

--- Localwire.Graphinder.Algorithms.Data/Algorithms/GeneticAlgorithm/GeneticAlgorithmEntity.cs
+++ Localwire.Graphinder.Algorithms.Data/Algorithms/GeneticAlgorithm/GeneticAlgorithmEntity.cs
@@ -1,7 +1,16 @@
 ﻿namespace Localwire.Graphinder.Algorithms.DataAccess.Algorithms.GeneticAlgorithm
 {
+    using EnumMappings.StrategyTypes;
+
     public class GeneticAlgorithmEntity : AlgorithmEntity
     {
-
+        public SelectionStrategyType SelectionStrategy { get; set; }
+        public CrossoverStrategyType CrossoverStrategy { get; set; }
+        public MutationStrategyType MutationStrategy { get; set; }
+        public uint InitialPopulationSize { get; set; }
+        public uint GenerationsToCome { get; set; }
+        public double CrossoverProbability { get; set; }
+        public double WithElitistSelection { get; set; }
+        public uint EliteSurvivors { get; set; }
     }
 }
--- Localwire.Graphinder.Algorithms.Data/Algorithms/GeneticAlgorithm/IndividualEntity.cs
+++ Localwire.Graphinder.Algorithms.Data/Algorithms/GeneticAlgorithm/IndividualEntity.cs
@@ -1,6 +1,12 @@
 ﻿namespace Localwire.Graphinder.Algorithms.DataAccess.Algorithms.GeneticAlgorithm
 {
-    public class IndividualEntity
+    using Graph;
+    using Problems;
+
+    public class IndividualEntity : BaseEntity
     {
+        public GraphEntity Graph { get; set; }
+        public ProblemEntity Problem { get; set; }
+        public bool[] CurrentSolution { get; set; }
     }
 }
