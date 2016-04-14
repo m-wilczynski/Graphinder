8120444886df9cdb95552b45468d1b2d447ff249
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-04-15 00:28:55 +0200

    Graph and Problem mappers stubs.

---
+++ Localwire.Graphinder.Algorithms.Data/Mappers/Graph/GraphMapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Localwire.Graphinder.Algorithms.DataAccess.Mappers.Graph
+{
+    using Core.Graph;
+    using Entities.Graph;
+
+    internal static class GraphMapper
+    {
+        public static Graph ToDomainModel(this GraphEntity entity)
+        {
+            return null;
+        }
+
+        public static GraphEntity ToEntityModel(this Graph model)
+        {
+            return null;
+        }
+    }
+}
---
+++ Localwire.Graphinder.Algorithms.Data/Mappers/Graph/NodeMapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Localwire.Graphinder.Algorithms.DataAccess.Mappers.Graph
+{
+    using Core.Graph;
+    using Entities.Graph;
+
+    internal static class NodeMapper
+    {
+        public static Node ToDomainModel(this NodeEntity entity)
+        {
+            return null;
+        }
+
+        public static NodeEntity ToEntityModel(this Node model)
+        {
+            return null;
+        }
+    }
+}
