da0c15996d64a6e762f0af7082553e9512351479
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-04-22 19:57:45 +0200

    Base for Algorithm domain db context.

--- Localwire.Graphinder.Algorithms.Data/Commands/UpdateProblem.cs
+++ Localwire.Graphinder.Algorithms.Data/Commands/UpdateProblem.cs
@@ -1,9 +1,11 @@
 ﻿namespace Localwire.Graphinder.Algorithms.DataAccess.Commands
 {
     using Core.Problems;
+    using EntityFramework;
 
     public class UpdateProblem
     {
+        internal AlgorithmContext Context = new AlgorithmContext();
 
         public UpdateProblem(IProblem problem)
         {
---
+++ Localwire.Graphinder.Algorithms.Data/EntityFramework/AlgorithmContext.cs
@@ -0,0 +1,36 @@
+﻿namespace Localwire.Graphinder.Algorithms.DataAccess.EntityFramework
+{
+    using System.Data.Entity;
+    using Core.Algorithms.SimulatedAnnealing;
+    using Entities.Algorithms;
+    using Entities.Algorithms.GeneticAlgorithm;
+    using Entities.Algorithms.SimulatedAnnealing;
+    using Entities.Graph;
+    using Entities.Problems;
+
+    internal class AlgorithmContext : DbContext 
+    {
+
+        public AlgorithmContext()
+        {
+           
+        }
+
+        public IDbSet<AlgorithmEntity> Algorithms { get; set; }
+        public IDbSet<GraphEntity> Graphs { get; set; }
+        public IDbSet<NodeEntity> Nodes { get; set; } 
+        public IDbSet<ProblemEntity> Problems { get; set; } 
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SimulatedAnnealingEntity>().ToTable("SimulatedAnnealing");
+            modelBuilder.Entity<GeneticAlgorithmEntity>().ToTable("GeneticAlgorithm");
+            modelBuilder.Entity<GraphEntity>().ToTable("Graph");
+            modelBuilder.Entity<NodeEntity>().ToTable("Node");
+            modelBuilder.Entity<MinimumVertexCoverEntity>().ToTable("MinimumVertexCover");
+        }
+
+    }
+}
